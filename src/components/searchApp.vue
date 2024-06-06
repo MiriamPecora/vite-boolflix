a371f0c070c0b366fb4f9542986c25c8ef0d0408
MiriamPecora <miriampecora97@gmail.com>
2024-06-06 19:15:10 +0200

    [UPD] Added 'original languages' string as flags icons

--- src/components/searchApp.vue
+++ src/components/searchApp.vue
@@ -2,54 +2,70 @@
 import { store } from '../store';
 import axios from 'axios';
 
-    export default {
-        name: "searchApp",
-        data() {
-            return {
-                store,
-                searched_media: "",
-            }
-        },
-        methods: {
-            getSearchedMedia() {
-                const url = `${this.store.infoAPI_movie.url}api_key=${this.store.api_key}&query=${this.searched_media}&language=${this.store.language}`;
-                axios
-                .get(url)
-                .then((response) => { 
-                    console.log(response);
-                    response.data.results.forEach(movie => {
-                        this.store.results.push(
-                            {
-                                title: movie.title,
-                                original_title: movie.original_title,
-                                original_language: movie.original_language,
-                                vote_average: movie.vote_average,
-                            }
-                        )
-                    });
-                    console.log(this.store.results)
+export default {
+    name: "searchApp",
+    data() {
+        return {
+            store,
+            searched_media: "",
+        }
+    },
+    methods: {
+        getSearchedMedia() {
+            const url = `${this.store.infoAPI_movie.url}api_key=${this.store.api_key}&query=${this.searched_media}&language=${this.store.language}`;
+            axios
+            .get(url)
+            .then((response) => { 
+                console.log(response);
+                response.data.results.forEach(movie => {
+                    const result = {
+                        title: movie.title,
+                        original_title: movie.original_title,
+                        original_language: movie.original_language,
+                        vote_average: movie.vote_average,
+                        flagUrl: this.getCorrectFlag(movie.original_language),
+                    };
+                    this.store.results.push(result);
                 });
-            },
+                console.log(this.store.results);
+            });
         },
-        created() {
+        getCorrectFlag(language) {
+            if (language === 'en') {
+                return './src/assets/img/english.png';
+            } else if (language === 'it') {
+                return './src/assets/img/italy.png';
+            };
         },
-    };
+    },
+    created() {
+    }
+};
 </script>
 
 <template>
     <div>
-        <input v-model="searched_media" type="text" placeholder="American Psycho">
+        <input v-model="searched_media" type="text" placeholder="Cerca un film">
         <button @click="getSearchedMedia">Cerca</button>
     </div>
 
     <div class="mediaCard">
-        <ul v-for="movie in store.results">
+        <ul v-for="(movie, index) in store.results" :key="index">
             <li> {{ movie.title }}</li>
             <ul>
-                <li> {{ movie.original_title }}</li>      
-                <li> {{ movie.original_language }}</li>      
-                <li> {{ movie.vote_average }}</li>      
+                <li> {{ movie.original_title }} </li>       
+                <li> <img class="flag" :src="movie.flagUrl" :alt="movie.original_language"> </li>    
+                <li> {{ movie.vote_average }} </li>      
             </ul>
         </ul>
     </div>
 </template>
+
+<style scoped>
+    .flag {
+    width: 25px;
+    height: 25px; 
+    display: inline-block;
+    }
+
+</style>
